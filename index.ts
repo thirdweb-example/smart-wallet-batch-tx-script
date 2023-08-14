748b74f15ee69f5f359d53e78f13356a70295c0d
ciaranightingale <ciara.nightingale@sky.com>
2023-08-14 19:10:22 +0100

    use LocalWalletNode & rename token contract

--- index.ts
+++ index.ts
@@ -2,11 +2,8 @@ import { config } from "dotenv";
 import { ThirdwebSDK } from "@thirdweb-dev/sdk";
 import { readFileSync } from "fs";
 import { Goerli } from "@thirdweb-dev/chains";
-import {
-  LocalWallet,
-  SmartWallet,
-  SmartWalletConfig,
-} from "@thirdweb-dev/wallets";
+import { SmartWallet, SmartWalletConfig } from "@thirdweb-dev/wallets";
+import { LocalWalletNode } from "@thirdweb-dev/wallets/evm/wallets/local-wallet-node";
 
 config();
 
@@ -24,9 +21,11 @@ const main = async () => {
 
   // Load or create personal wallet
   // here we generate LocalWallet that will be stored in wallet.json
-  const personalWallet = new LocalWallet();
-  await personalWallet.generate();
-  await personalWallet.connect();
+  const personalWallet = new LocalWalletNode();
+  await personalWallet.loadOrCreate({
+    strategy: "encryptedJson",
+    password: "password",
+  });
 
   const personalWalletAddress = await personalWallet.getAddress();
   console.log("Personal wallet address:", personalWalletAddress);
@@ -54,19 +53,19 @@ const main = async () => {
   console.log("balance:", (await sdk.wallet.balance()).displayValue);
 
   // Claim a ERC20 token
-  const contract = await sdk.getContract(
+  const tokenContract = await sdk.getContract(
     "0xc54414e0E2DBE7E9565B75EFdC495c7eD12D3823" // TokenDrop on goerli
   );
   const editionContract = await sdk.getContract(
     "0x8096C71f400984C3C1B7F3a79Ab0C0EaC417b91c"
   ); // EditionDrop on goerli
-  const tokenBalance = await contract.erc20.balance();
+  const tokenBalance = await tokenContract.erc20.balance();
   const editionBalance = await editionContract.erc1155.balance(0);
   console.log("ERC20 token balance:", tokenBalance.displayValue);
   console.log("Edition NFT balance:", editionBalance.toNumber());
 
   // Execute multiple transactions at once
-  const tx1 = await contract.erc20.claim.prepare(1);
+  const tx1 = await tokenContract.erc20.claim.prepare(1);
   const tx2 = await editionContract.erc1155.claim.prepare(0, 1);
   const transactions = [tx1, tx2];
 
@@ -75,7 +74,7 @@ const main = async () => {
     "Claimed 1 ERC20 token & 1 Edition NFT, tx hash:",
     batchTx.receipt.transactionHash
   );
-  const tokenBalanceAfter = await contract.erc20.balance();
+  const tokenBalanceAfter = await tokenContract.erc20.balance();
   const editionBalanceAfter = await editionContract.erc1155.balance(0);
   console.log("ERC20 token balance:", tokenBalanceAfter.displayValue);
   console.log("Edition NFT balance:", editionBalanceAfter.toNumber());
